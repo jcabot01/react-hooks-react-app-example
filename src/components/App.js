106a2fd34b631aacbc3d3edaafb761e141f585a1
JonCabot <jfc0053@gmail.com>
2022-01-25 17:05:27 -0600

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { format } from "date-fns";
-import ExampleComponent from "./ExampleComponent";
+import React from "react"; //allows React libraries to be used.  In v.17 we don't need this statement
+// import { format } from "date-fns"; //uses the 'format' from the dat-fns package
+import ExampleComponent from "./ExampleComponent"; //allows ExampleComponent.js file to be used in the App
+import TestComponent from "./TestComponent";
 
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <h1>Now</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -14,8 +15,9 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <TestComponent />
     </div>
   );
 }
-
+//we can only have one default export per file, this is the main thing we want to export
 export default App;
